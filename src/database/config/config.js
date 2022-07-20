3c46b4a2cac09c9078dcac2ad304e3d3065324c4
C.Swame <caio_swame@hotmail.com>
2022-07-20 00:36:35 -0300

    chore[npx sequelize init]: creates innitial files and set some configs
    the default sequelize file 'config' was updated to work dynamically

---
+++ src/database/config/config.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const environment = process.env.NODE_ENV || 'test';
+
+const suffix = {
+  dev: '-dev',
+  development: '-dev',
+  test: '-test',
+  production: '',
+};
+
+const options = {
+  host: process.env.HOSTNAME || process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || '3306',
+  database: `${process.env.DB_NAME || 'trading-app'}${
+    suffix[environment] || suffix.test
+  }`,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  dialect: process.env.DB_DIALECT || 'mysql',
+  // dialectOptions: {
+  // timezone: 'Z',
+  // },
+  timezone: '-03:00',
+  logging: process.env.DEBUG !== 'false',
+};
+
+module.exports = {
+  development: {
+    ...options,
+  },
+  test: {
+    ...options,
+  },
+  production: {
+    ...options,
+  },
+};
