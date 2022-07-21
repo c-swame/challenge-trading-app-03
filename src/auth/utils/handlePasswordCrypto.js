1c79c20ead67e8391bec19c2bc3badc1ca43e34c
C.Swame <caio_swame@hotmail.com>
2022-07-21 04:44:39 -0300

    chore[src auth utils]: creates a function to handle password encrypting
    this module export a module export an object with two functions:
    'encrypt': recives one argument and returns a 'hex' hash;
    'decrypt': recives a hex hash and returns its original data.

---
+++ src/auth/utils/handlePasswordCrypto.js
@@ -0,0 +1,30 @@
+const Crypto = require('crypto');
+
+require('dotenv').config();
+
+const {
+  PASSWORD_CRYPTO_ALG,
+  PASSWORD_CRYPTO_IV,
+  PASSWORD_CRYPTO_KEY,
+} = process.env;
+
+const encrypt = (data) => {
+  const cipher = Crypto
+    .createCipheriv(PASSWORD_CRYPTO_ALG, PASSWORD_CRYPTO_KEY, PASSWORD_CRYPTO_IV);
+  const encrypted = cipher.update(JSON.stringify(data), 'utf-8', 'hex');
+  return encrypted;
+};
+
+const decrypt = (dataHash) => {
+  const decipher = Crypto
+    .createDecipheriv(PASSWORD_CRYPTO_ALG, PASSWORD_CRYPTO_KEY, PASSWORD_CRYPTO_IV);
+  const decrypted = JSON.parse(decipher.update(dataHash, 'hex', 'utf-8'));
+  return decrypted;
+};
+
+// metodo tem que ser melhorado de forma que a senha de duas pessoas diferentes,
+//   ainda que iguais não tenham o mesmo hash .-.
+
+console.log(encrypt('qwertyas'));
+
+module.exports = { encrypt, decrypt };
