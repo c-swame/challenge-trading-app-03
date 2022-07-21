e0ad94b681d8ad95540fceb3b5cba775017ec724
C.Swame <caio_swame@hotmail.com>
2022-07-21 18:31:30 -0300

    chores[src database seeders user]: creates an initial file to seed the table 'Users'

---
+++ src/database/seeders/20220720204044-users.js
@@ -0,0 +1,31 @@
+module.exports = {
+  async up(queryInterface, _Sequelize) {
+    queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          nome: 'user1',
+          email: 'email1@email.com',
+          saldo: 1000,
+          cpf: '123.456.789-00',
+          telefone: '9999999999',
+          passwordHash: '854420d77b7998aefe',
+          // password
+        },
+        {
+          nome: 'user2',
+          email: 'email2@email.com',
+          saldo: 1234567.123456789,
+          cpf: '123.456.789-01',
+          passwordHash: '960764863c3bd7f8ba27',
+          // password
+        },
+      ],
+      {},
+    );
+  },
+
+  async down(queryInterface, _Sequelize) {
+    queryInterface.bulkDelete('Users', null, {});
+  },
+};
