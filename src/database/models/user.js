6e9df62c9d0ed6d89ec351d1318ce4b69fdd3d59
C.Swame <caio_swame@hotmail.com>
2022-07-21 18:29:03 -0300

    chore[src database models user]: creates a model to the table 'Users'

---
+++ src/database/models/user.js
@@ -0,0 +1,57 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    // static associate(models) {
+    //   // define association here
+    // }
+  }
+  User.init({
+    codeClient: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nome: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+      },
+    },
+    telefone: DataTypes.STRING,
+    saldo: DataTypes.DECIMAL,
+    cpf: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    admin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    passwordHash: DataTypes.STRING,
+    password: {
+      type: DataTypes.VIRTUAL,
+      validate: {
+        len: [8, 32],
+      },
+    },
+    criadoEm: DataTypes.DATE,
+    atualizadoEm: DataTypes.DATE,
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+    timestamps: false,
+  });
+  return User;
+};
