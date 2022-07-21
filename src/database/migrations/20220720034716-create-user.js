b33eeebb237faeff0b64aabef1fda5ec7b8d8f18
C.Swame <caio_swame@hotmail.com>
2022-07-21 18:30:05 -0300

    chore[src database migrations] creates a migration to the table 'Users'

---
+++ src/database/migrations/20220720034716-create-user.js
@@ -0,0 +1,57 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      telefone: {
+        type: Sequelize.STRING,
+      },
+      passwordHash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      saldo: {
+        type: Sequelize.DECIMAL(50, 2),
+        // ??????Como fica a questão do arredondamento??????
+      },
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      admin: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      criadoEm: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+        noUpdate: true,
+      },
+      atualizadoEm: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
