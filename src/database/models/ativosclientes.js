1b004095717105354bccab2188d7437dbe666f38
C.Swame <caio_swame@hotmail.com>
2022-07-22 18:46:22 -0300

    chore[src database models ativosclientes]: creates an initial model to the table 'AtivosClientes'

---
+++ src/database/models/ativosclientes.js
@@ -0,0 +1,27 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class AtivosClientes extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    // static associate(models) {
+    //   // define association here
+    // }
+  }
+  AtivosClientes.init(
+    {
+      codCliente: DataTypes.NUMBER,
+      codAtivo: DataTypes.NUMBER,
+    },
+    {
+      sequelize,
+      modelName: 'AtivosClientes',
+      tableName: 'AtivosClientes',
+      timestamps: false,
+    },
+  );
+  return AtivosClientes;
+};
