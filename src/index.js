cf95a6b5b07b011a6b28d22a1436d3940c1f4dba
C.Swame <caio_swame@hotmail.com>
2022-07-22 11:25:08 -0300

    feat: add a new routes 'get /users' that returns all the registered users

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+
+require('./utils/getEnvVar').config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(
+  `Server is running on PORT: ${PORT}`,
+));
