cfe22be95ad0b3bd04a305b6df5d2068ef48d026
C.Swame <caio_swame@hotmail.com>
2022-07-23 14:16:50 -0300

    chore[src services assetsServices getAssetsByUserId]: creates a service that returns a user assets
    must recive an codClient as arg;
    returns an array with 'codClient', 'codAtivo', 'qtdeAtivo' (owned by the user) and 'valor'

---
+++ src/services/assetsServices/getAssetsByUserId.js
@@ -0,0 +1,15 @@
+const { sequelize } = require('../../database/models');
+
+module.exports = async (userId, mockTest = {}) => {
+  const query = `
+SELECT ac.codCliente, ac.codAtivo, ac.quantidade qtdeAtivo, a.Valor FROM trading_app_dev.AtivosClientes as ac
+JOIN trading_app_dev.Ativos as a
+ON ac.codAtivo = a.codAtivo
+WHERE codCliente = :userId;
+  `
+
+  const [userAssets] = mockTest.modelReturn || await sequelize.query(query, {
+    replacements: { userId },
+  });
+  return userAssets;
+};
