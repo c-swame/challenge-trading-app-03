52ffd313645424cba5a063c424f59cb9ccf2716c
C.Swame <caio_swame@hotmail.com>
2022-07-23 12:02:50 -0300

    chore[src test unit assets assetsControllers testAssetsByUserIdController]:
    add cases to when the request or service throw some error

--- src/tests/unit/assets/assetsControllers/testGetAssetsByUserIdController.test.js
+++ src/tests/unit/assets/assetsControllers/testGetAssetsByUserIdController.test.js
@@ -59,8 +59,90 @@ describe(`test the getAssetsByUderId controller when it recives a valid data fro
   });
 
   it(`test if the return the expected json content`, async() => {
-    console.log('aaaaaaaaaaaaaa', expectedJsonReturn);
     await assetsControllers.getAssetsByUserId(mockReq, mockResp, mockNext);
     expect(mockResp.json).calledWith(expectedJsonReturn);
+  });
+});
+
+describe(`test the getAssetsByUderId controller when it recives no token`, async () => {
+  const expectedJsonReturn = {
+    data: [
+      {
+        codCliente: 2,
+        codAtivo: 2,
+        quantidade: 2,
+      },
+      {
+        codCliente: 2,
+        codAtivo: 1,
+        quantidade: 1,
+      },
+    ],
+    token: 'JWT',
+  };
+
+  const expectedStatusReturn = statusCode.OK;
+
+  
+  const mockResp = {};
+  const mockReq = {};
+  let mockNext = () => true;
+
+  before(() => {
+    mockReq.body = {};
+    mockReq.params = {
+      codCliente: 2
+    }
+    mockReq.headers = {
+      authorization: undefined
+    }
+
+    mockResp.status = sinon.stub().returns(mockResp);
+    mockResp.json = sinon.stub().returns();
+    mockNext = sinon.stub().returns(true);
+
+    sinon.stub(assetsService, "getAssetsByUserId").resolves(expectedJsonReturn.data);
+  });  
+
+  after(() => {
+    assetsService.getAssetsByUserId.restore();
+  });
+
+  it(`test if the nextFunction is called`, async() => {
+    await assetsControllers.getAssetsByUserId(mockReq, mockResp, mockNext);
+    expect(mockNext).calledOnce;
+    expect(mockNext).calledWith(sinon.match({message: 'This request must contain a token'}));
+  });
+});
+
+describe(`test the getAssetsByUderId controller when it recives a token but the service trhow an error`, async () => {
+  const mockResp = {};
+  const mockReq = {};
+  let mockNext = () => true;
+
+  before(() => {
+    mockReq.body = {};
+    mockReq.params = {
+      codCliente: 2
+    }
+    mockReq.headers = {
+      authorization: 'JWT'
+    }
+
+    mockResp.status = sinon.stub().returns(mockResp);
+    mockResp.json = sinon.stub().returns();
+    mockNext = sinon.stub().returns(true);
+
+    sinon.stub(assetsService, "getAssetsByUserId").rejects({message: 'cowabunga'});
+  });  
+
+  after(() => {
+    assetsService.getAssetsByUserId.restore();
+  });
+
+  it(`test if the nextFunction is called`, async() => {
+    await assetsControllers.getAssetsByUserId(mockReq, mockResp, mockNext);
+    expect(mockNext).calledOnce;
+    expect(mockNext).calledWith(sinon.match({message: 'cowabunga'}));
   });
 });
