775754c550c4620ee6771ce50203cbd6ed498fdf
C.Swame <caio_swame@hotmail.com>
2022-07-22 23:05:14 -0300

    chore[src tests assets assetsServices testGetAssetsByUserId]

---
+++ src/tests/unit/assets/assetsServices/testGetAssetsByUserId.test.js
@@ -0,0 +1,45 @@
+const sinon = require('sinon');
+const { expect, use } = require('chai');
+const sinonChai = require("sinon-chai");
+
+const statusCode = require('../../../../utils/statusCode');
+
+use(sinonChai);
+
+const { getAssetsByUserId } = require('../../../../services/assetsServices');
+
+describe(`test the getProductById.js function when it recives
+a valid data from controller and from model`, async () => {
+  const mockTest = {
+    modelReturn: [
+    {
+      codCliente: 2,
+      codAtivo: 2,
+      quantidade: 2,
+    },
+    {
+      codCliente: 2,
+      codAtivo: 1,
+      quantidade: 1,
+    },
+  ],
+};
+
+  const expectedReturn = [
+    {
+      codCliente: 2,
+      codAtivo: 2,
+      quantidade: 2,
+    },
+    {
+      codCliente: 2,
+      codAtivo: 1,
+      quantidade: 1,
+    },
+  ];
+
+  it(`test if the return is as expected`, async() => {
+    const response = await getAssetsByUserId(2, mockTest);
+    expect(response).to.deep.equal(expectedReturn);
+  });
+});
