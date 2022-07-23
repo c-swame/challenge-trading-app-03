20a01ed928fb851531e3f7aaba6c914a6b124de4
C.Swame <caio_swame@hotmail.com>
2022-07-23 14:57:32 -0300

    style[src routes ativos]: rename route 'get ativos/user/:userId' to 'get ativos/clientes/:userId'

--- src/routes/assetsRouter.js
+++ src/routes/assetsRouter.js
@@ -4,6 +4,6 @@ const assetsController = require('../controllers/assetsControllers');
 
 const router = express.Router({ mergeParams: true });
 
-router.get('/user/:userId', assetsController.getAssetsByUserId);
+router.get('/clientes/:userId', assetsController.getAssetsByUserId);
 
 module.exports = router;
