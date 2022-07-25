2c30182328e22107057710ad569b8337bf69da22
C.Swame <caio_swame@hotmail.com>
2022-07-24 21:09:55 -0300

    feat[routes]: creates a route 'post /investimentos/vender' to sell assets

--- src/routes/transactionsRouter.js
+++ src/routes/transactionsRouter.js
@@ -6,5 +6,6 @@ const router = express.Router({ mergeParams: true });
 
 // !!!!!!!!! falta verificar se o token da pessoa compradora é o mesmo do codClient da requisição
 router.post('/investimentos/comprar', transactionsController.buyAssets);
+router.post('/investimentos/vender', transactionsController.sellAssets);
 
 module.exports = router;
