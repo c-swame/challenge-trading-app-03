186f0274076e35d25c2d278b6674d9615fea6530
C.Swame <caio_swame@hotmail.com>
2022-07-24 23:26:12 -0300

    fix[services]

--- src/services/transactions/postDeposit.js
+++ src/services/transactions/postDeposit.js
@@ -7,8 +7,10 @@ module.exports = async (transactionData, mockTest = {}) => {
 
   // depositos não serão salvos na tablea transações por hora, pois isso iria requer uma edição na model ou no esquema de tablas, algo que eu não conseguiria fazer no momento para entregar a tempo;
 
-  const depositData = mockTest.modelReturn || await User.update(
-    { saldo: valor, codOperacao: 'deposito' },
+  const userData = await User.findByPk(codCliente);
+
+  const [depositData] = mockTest.modelReturn || await User.update(
+    { saldo: (+userData.saldo + +valor), codOperacao: 'deposito' },
     { where: { codCliente } },
   );
 
