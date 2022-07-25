ce00e8d6aac1a92983e9ff8c37c437825f1e0f38
C.Swame <caio_swame@hotmail.com>
2022-07-24 21:47:41 -0300

    chore[src services transactions]: creates a service do deposit

--- src/services/transactions/index.js
+++ src/services/transactions/index.js
@@ -1,7 +1,9 @@
 const postBuyinfAssets = require('./postBuyingAssets');
 const sellAssets = require('./sellAssets');
+const postDeposit = require('./postDeposit');
 
 module.exports = {
   postBuyinfAssets,
   sellAssets,
+  postDeposit,
 };
---
+++ src/services/transactions/postDeposit.js
@@ -0,0 +1,16 @@
+const {
+  User,
+} = require('../../database/models');
+
+module.exports = async (transactionData, mockTest = {}) => {
+  const { codCliente, valor } = transactionData;
+
+  // depositos não serão salvos na tablea transações por hora, pois isso iria requer uma edição na model ou no esquema de tablas, algo que eu não conseguiria fazer no momento para entregar a tempo;
+
+  const depositData = mockTest.modelReturn || await User.update(
+    { saldo: valor, codOperacao: 'deposito' },
+    { where: { codCliente } },
+  );
+
+  return depositData;
+};
