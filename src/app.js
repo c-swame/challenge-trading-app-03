a884843dcfc84b0d81b58e77bfafbe489fbefb15
C.Swame <caio_swame@hotmail.com>
2022-09-20 09:25:40 -0300

    feat[app.js routes /docs]: Creates a route to acess the swagger documentation; Also anable cors;

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 require('express-async-errors');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 const errorMiddleware = require('./middlewares/errorMiddleware/errorMiddleware.middleware');
 const appRoutes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(appRoutes);
 
 app.get('/', (_req, resp) => resp.send('<p>Bem vindo à api trading app!!</p>'));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorMiddleware);
 
